e1f85d6438b5fb092c9a6c92a46b19733fc9f9c8
mal-efficient <johnycsh@gmail.com>
2020-01-23 19:15:15 -0500

    code de-duplication

--- math_dbm_util.py
+++ math_dbm_util.py
@@ -7,55 +7,6 @@ import sys
 # |  1e-6          | 1000           |  1.0             | 
 ########################################################
 
-def milliwatt_to_picowatt(_in):
-    return 1e+9*_in
-def microwatt_to_picowatt(_in):
-    return 1e+6*_in
-def nanowatt_to_picowatt(_in):
-    return 1e+3*_in 
-def picowatt_to_picowatt(_in):
-    return 1e+0*_in 
-
-def milliwatt_to_nanowatt(_in):
-    return 1e+6*_in
-def microwatt_to_nanowatt(_in):
-    return 1e+3*_in
-def nanowatt_to_nanowatt(_in):
-    return 1e+0*_in 
-def picowatt_to_nanowatt(_in):
-    return 1e-3*_in 
-
-def milliwatt_to_microwatt(_in):
-    return 1e+3*_in
-def microwatt_to_microwatt(_in):
-    return 1e+0*_in
-def nanowatt_to_microwatt(_in):
-    return 1e-3*_in 
-def picowatt_to_microwatt(_in):
-    return 1e-6*_in 
-
-def milliwatt_to_milliwatt(_in):
-    return 1e+0*_in
-def microwatt_to_milliwatt(_in):
-    return 1e-3*_in
-def nanowatt_to_milliwatt(_in):
-    return 1e-6*_in 
-def picowatt_to_milliwatt(_in):
-    return 1e-9*_in 
-
-#Note: If floating point precision becomes a problem, consider converting dBm -> picoWatts using fixed precision math; scale other calculations off results
-def dBm_to_milliwatt(sig):
-    x = sig / 10
-    ret =  math.pow(10,x+3)
-    #print("dBm_to_milliwatt(%d) = %s" % (sig, ret))
-    return ret 
-
-def milliwatt_to_dBm(mw_in):
-    if (mw_in < 0.00000001) and (mw_in > -0.00000001):
-        print("milliwatt_to_dBm special case: passed ~0.0. Returning 1")
-        return 1
-    ret =  math.log(float(mw_in), 10) * 10.0  - 30
-    return ret
 
 def dBm_as_percent(dBm_a, dBm_b):
     """ Express dBm_a as percentage of dBm_b"""
